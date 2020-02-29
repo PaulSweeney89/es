583c6a738d743f0a7cf44d2d300cb88bd5287aa5
PaulSweeney89 <g00229879@gmit.ie>
2020-02-29 16:38:28 +0000

    code working to calc number of e's

--- es.py
+++ es.py
@@ -1,12 +1,10 @@
 # program that reads in a text file & counts the number of e's it contains.
 
-for n in range(0, 256):
-    print(f"{n:3} {n:08b} {chr(n)}")
-
 with open("moby-dick.txt", "r") as reader:
-    for i in reader:
-        value_count = 0
-        if f"{chr(i)}" == "e":
-            value_count = value_count + 1 
-
-        print(value_count)
+    read_file = reader.read()
+    e_count = 0
+    for i in read_file:
+        if i == "e":
+            e_count = e_count + 1
+    print(e_count)
+        
