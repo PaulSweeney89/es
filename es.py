7fd3fde78368ed7214e5cbc67a575c4f292d6381
PaulSweeney89 <g00229879@gmit.ie>
2020-03-02 22:01:19 +0000

    updated to take text file name as command line argument

--- es.py
+++ es.py
@@ -1,18 +1,17 @@
 # program that reads in a text file from a inputted file directory & counts the number of e's it contains.
 # sample file directory: /home/paul/Desktop/es/moby-dick.txt
 
-#input text file directory to be read
-txt_file = input("please input text file directory ")
-
+import sys                                  # import sys module
 
 # use 'with' statement to open text file & automatically close file after statement is executed.
-with open(txt_file, "r") as reader:        # open file in read mode 'r'. 
+file_name = sys.argv[1]                     # input text file name assigned as a command line argument
+with open(file_name, "r") as reader:        # open file in read mode 'r'. 
     read_file = reader.read()              # read file
     
     e_count = 0                            # initiate e counter with e_count = 0.
     
     for i in read_file:                    # 'for' loop through every character 'i' in read file.
-        if i == "e":                       # 'if' statement 'i' = 'e' is True
+        if i == "e":                       # 'if' statement 'i' = 'e' is True (lowecase)
             e_count = e_count + 1          # add 1 to e counter.
 
     # print final e_count value when for loop has gone through all characters in read text file.
